40baf1a499eaaceb8382aeaa6992c0a829ccc244
Collin OConnell <ococncol@gmail.com>
2020-04-08 16:01:26 -0400

    starting team

--- index.js
+++ index.js
@@ -2,12 +2,43 @@ const { ApolloServer, gql } = require('apollo-server');
 const { GraphQLScalarType } = require('graphql');
 
 const typeDefs = gql`
+  type Team {
+    id: ID!
+    name: String!
+    league: String!
+    W: Int
+    D: Int
+    L: Int
+    Latest: String
+  }
+
+  type Query {
+    getTeams: [Team]
+    getTeam: Team
+  }
 `
 
+const teams = [
+  {
+    id: "1",
+    name: "Detroit City FC",
+    league: "NISA",
+    W: 2,
+    L: 0,
+    D: 0,
+    Latest: "W"
+  }
+]
 
 const resolvers = {
-  Query: {},
-  Mutation: {},
+  Query: {
+    getTeams: (obj, arg, context, info) => teams,
+    getTeam: (obj, { id }, context, info) => {
+      const filterTeam = teams.filter(t => t.id === id)
+      return filterTeam[0];
+    }
+  },
+  // Mutation: {},
   // Player: {
   //   team: (obj, arg, context, info) => {
   //     const filteredTeam = teams.filter(team => team.id == obj.team)
