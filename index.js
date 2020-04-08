8bdc756987f3f56b9d31d711ccb08e09c643385b
Collin OConnell <ococncol@gmail.com>
2020-04-08 10:55:30 -0400

    creating graphql

---
+++ index.js
@@ -0,0 +1,55 @@
+const { ApolloServer, gql } = require('apollo-server');
+
+const typeDefs = gql`
+  enum Postion {
+    GOALKEEPER
+    FORWARD
+    DEFENDER
+  }
+  type Player {
+    id: ID!
+    name: String!
+    position: Postion
+    squadNumber: Int
+    bio: String
+  }
+
+  type Query {
+    players: [Player]
+    player(id: ID): Player
+  }
+`
+
+const players = [
+  {
+    id: 1,
+    name: "Nate Stinewasher",
+    position: "GOALKEEPER",
+    squadNumber: 0,
+    bio: "This is a bio",
+  },
+  {
+    id: 2,
+    name: "Blake Goodman",
+    position: "FORWARD",
+    squadNumber: 10,
+    bio: "This is a bio",
+  },
+];
+
+const resolvers = {
+  Query: {
+    players: () => players,
+    player: (obj, arg, context, info) => {
+      const { id } = arg;
+      const foundPlayer = players.find(p => p.id == id);
+      return foundPlayer;
+    }
+  },
+}
+
+const server = new ApolloServer({ typeDefs, resolvers })
+
+server.listen().then(({ url }) => {
+  console.log(`Server started at ${url}`)
+});
